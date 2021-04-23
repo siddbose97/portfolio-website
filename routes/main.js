8315eaf9cdaf33d05950d21fd689a5c9b1b59a2d
Siddhartha Bose <siddbose97@gmail.com>
2021-04-23 19:12:44 +0800

    day 3 half done

--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,21 @@
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
+const ProjectController = require('../controllers/ProjectController')
 
 router.get('/', (req, res) => {
-
     const data = req.context //{page: ...}
 
-    res.render('landing', data)
+    const projectCtr = new ProjectController()
+    projectCtr.get()
+    .then(projects => {
+        data['projects'] = projects
+        res.render('landing', data)
+    })
+    .catch(err => {
+        res.send('Technical Difficulties'+err.message)
+    })
+
+
 })
 
 module.exports = router
